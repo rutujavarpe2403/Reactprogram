b46f32330a569a56eced78912ed306b480f392e7
rutujavarpe2403 <rutujavarpe2403@gmail.com>
2025-08-19 20:45:28 +0530

    Added project files

---
+++ helloworld/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import User from './user.jsx'
+
+function App() {
+  return (
+    <div>
+      <User />
+    </div>
+  )
+}
+
+export default App
---
+++ helloworld/src/user.jsx
@@ -0,0 +1,7 @@
+function user(){
+    return(
+        <h1>hello world welcome to react  project</h1>
+
+    )
+}
+export default user;
