9ed5227f507a834410d15f5e04ed024d16f1a984
rutujavarpe2403 <rutujavarpe2403@gmail.com>
2025-08-21 00:15:18 +0530

    Added helloworld folder

--- helloworld/src/App.jsx
+++ helloworld/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -13,3 +14,27 @@ function App() {
 }
 
 export default App
+
+import React from 'react';
+import Clock from './clock.jsx';
+//import React, { useState } from 'react';
+import { useState } from 'react';
+
+
+function App() {
+  const [color, setColor] = useState('red')
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <select onChange={(e) => setColor(e.target.value)}>
+        <option value={"red"}>red</option>
+        <option value={"green"}>green</option>
+        <option value={"blue"}>blue</option>
+      </select>
+      <Clock color={color} />
+    </div>
+  );
+}
+
+export default App;
+
